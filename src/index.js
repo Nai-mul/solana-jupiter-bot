f2efea06000bdc6215e561d52f5d3a15e6ff2d12
Naimul <64520358+Nai-mul@users.noreply.github.com>
2024-06-11 06:11:07 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,49 +4,54 @@ const React = require("react");
 const importJsx = require("import-jsx");
 const { render } = require("ink");
 const meow = require("meow");
+const { checkForEnvFile, checkWallet, checkArbReady, logExit } = require("./utils");
+const chalk = require("chalk");
 
-// check for .env file
-const { checkForEnvFile, checkWallet, checkArbReady } = require("./utils");
+// Ensure .env file is present
 checkForEnvFile();
 
+// Load environment variables from .env file
 require("dotenv").config();
 
+// Check wallet configuration
 checkWallet();
 
 const isArbReady = async () => {
     try {
-        // Display the message
+        // Check if arbitrage is ready
         await checkArbReady();
-        return true; // If checkArbReady completes without errors, return true
+        return true; // Return true if checkArbReady completes without errors
     } catch (error) {
-        spinner.text = chalk.black.bgRedBright(
-            `\n${error.message}\n`
-        );
+        // Display error message and exit process
+        console.error(chalk.black.bgRedBright(`\n${error.message}\n`));
         logExit(1, error);
-        process.exit(1); // Exit the process if there's an error
+        process.exit(1);
     }
 };
 
+// Check if arbitrage is ready and proceed if true
 isArbReady().then((arbReady) => {
-	if (!arbReady) {
-        process.exit(1); // Exit the process if ARB is not ready
+    if (!arbReady) {
+        process.exit(1); // Exit if ARB is not ready
     }
 });
 
+// Import and render the wizard component
 const wizard = importJsx("./wizard/index");
 
 const cli = meow(`
-	Usage
-	  $ solana-jupiter-bot
+    Usage
+      $ solana-jupiter-bot
 
-	Options
-		--name  Your name
+    Options
+        --name  Your name
 
-	Examples
-	  $ solana-jupiter-bot --name=Jane
-	  Hello, Master
+    Examples
+      $ solana-jupiter-bot --name=Jane
+      Hello, Master
 `);
 
 console.clear();
 
+// Render the React component and wait until it exits
 render(React.createElement(wizard, cli.flags)).waitUntilExit();
